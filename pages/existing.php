ecc1b4b194c43cc4db9a738f9c43a312ff4b5a37
Emoblazz <emoblazz@gmail.com>
2017-09-05 15:02:32 +0800

    Existing update

--- pages/existing.php
+++ pages/existing.php
@@ -97,6 +97,70 @@
                                         </td>
                                     </tr>
                             <!-- Modal -->
+                            <div class="modal fade" id="delete<?php echo $id;?>" tabindex="-6" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                 <form method="post" action="taker_update.php">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="myModalLabel">Update Taker's Details</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!--row-->
+                                            <input class="form-control" name="id" type="hidden" value="<?php echo $id;?>">
+                                            <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="form-group">
+                                                       <label>Last Name</label>
+                                                        <input class="form-control" name="last" placeholder="Last Name" value="<?php echo $last;?>">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!--row-->
+                                            <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="form-group">
+                                                       <label>First Name</label>
+                                                        <input class="form-control" name="first" placeholder="First Name" value="<?php echo $first;?>">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!--row-->
+                                             <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="form-group">
+                                                       <label>Birthday</label>
+                                                        <input class="form-control" type="date" name="bday" placeholder="Birthday" value="<?php echo $bday;?>">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!--row-->        
+                                             <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="form-group">
+                                                       <label>Status</label>
+                                                        <select class="form-control" name="status">
+                                                         <option><?php echo $status;?></option>
+                                                         <option>Active</option>
+                                                         <option>Inactive</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!--row-->
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn btn-primary" name="edit">Save</button>
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div>
+                            </form>
+                            <!-- /.modal -->
+                            <!-- Modal -->
                             <div class="modal fade" id="edit<?php echo $id;?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
@@ -298,70 +362,7 @@
                                 <!-- /.modal-dialog -->
                             </div>
                             <!-- /.modal -->                                        
-                            <!-- Modal -->
-                            <div class="modal fade" id="delete<?php echo $id;?>" tabindex="-6" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                 <form method="post" action="taker_update.php">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                            <h4 class="modal-title" id="myModalLabel">Update Taker's Details</h4>
-                                        </div>
-                                        <div class="modal-body">
-                                            <!--row-->
-                                            <input class="form-control" name="id" type="hidden" value="<?php echo $id;?>">
-                                            <div class="row">
-                                                <div class="col-lg-8">
-                                                    <div class="form-group">
-                                                       <label>Last Name</label>
-                                                        <input class="form-control" name="last" placeholder="Last Name" value="<?php echo $last;?>">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!--row-->
-                                            <div class="row">
-                                                <div class="col-lg-8">
-                                                    <div class="form-group">
-                                                       <label>First Name</label>
-                                                        <input class="form-control" name="first" placeholder="First Name" value="<?php echo $first;?>">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!--row-->
-                                             <div class="row">
-                                                <div class="col-lg-8">
-                                                    <div class="form-group">
-                                                       <label>Birthday</label>
-                                                        <input class="form-control" type="date" name="bday" placeholder="Birthday" value="<?php echo $bday;?>">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!--row-->        
-                                             <div class="row">
-                                                <div class="col-lg-8">
-                                                    <div class="form-group">
-                                                       <label>Status</label>
-                                                        <select class="form-control" name="status">
-                                                         <option><?php echo $status;?></option>
-                                                         <option>Active</option>
-                                                         <option>Inactive</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!--row-->
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="submit" class="btn btn-primary" name="edit">Save</button>
-                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                        </div>
-                                    </div>
-                                    <!-- /.modal-content -->
-                                </div>
-                                <!-- /.modal-dialog -->
-                            </div>
-                            </form>
-                            <!-- /.modal -->                                        
+                                                                    
 <?php }?>                                    
                                     
                                    
